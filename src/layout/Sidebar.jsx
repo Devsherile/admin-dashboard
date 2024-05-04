d0f041ef8b340454f22ac4bd8adf0b7c629894fd
Sherile <sherilecheptoo1@gmail.com>
2024-05-05 01:28:22 +0300

    fix

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,68 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaCog,
   FaComments,
   FaEdit,
   FaEnvelope,
   FaFileInvoice,
-  FaHome, 
+  FaHome,
   FaUsers,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Sidebar() {
+  const [activeItem, setActiveItem] = useState("Dashboard"); 
+  const handleItemClick = (item) => {
+    setActiveItem(activeItem === item ? null : item); // Toggle active state
+  };
+
   return (
     <div className="text-gray-800 bg-white h-screen w-60 border-r border-r-slate-400">
       <div className="p-4">
         <h1 className="text-2xl font-bold">Sidebar</h1>
       </div>
-      <ul className="py-4">
+      <ul className="py-4 space-y-2">
         <Link
           to="/dashboard"
-          className="px-4 items-center justify-start pl-10  gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full"
+          onClick={() => handleItemClick("Dashboard")}
+          className={`px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full ${
+            activeItem === "Dashboard" && "bg-gray-700 text-white"
+          }`}
         >
           <FaHome />
           <li>Dashboard</li>
         </Link>
         <Link
           to="/subscribers"
-          className="px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full"
+          onClick={() => handleItemClick("Subscribers")}
+          className={`px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full ${
+            activeItem === "Subscribers" && "bg-gray-700 text-white"
+          }`}
         >
           <FaEnvelope />
-
           <li>Subscribers</li>
         </Link>
         <Link
           to="/blog"
-          className="px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full"
+          onClick={() => handleItemClick("Blog")}
+          className={`px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full ${
+            activeItem === "Blog" && "bg-gray-700 text-white"
+          }`}
         >
           <FaEdit />
           <li>Blog</li>
         </Link>
         <Link
           to="/comments"
-          className="px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full"
+          onClick={() => handleItemClick("Comments")}
+          className={`px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full ${
+            activeItem === "Comments" && "bg-gray-700 text-white"
+          }`}
         >
           <FaComments />
           <li>Comments</li>
         </Link>
         <Link
           to="/users"
-          className="px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full"
+          onClick={() => handleItemClick("Users")}
+          className={`px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full ${
+            activeItem === "Users" && "bg-gray-700 text-white"
+          }`}
         >
           <FaUsers />
           <li>Users</li>
         </Link>
         <Link
           to="/invoices"
-          className="px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full"
+          onClick={() => handleItemClick("Invoices")}
+          className={`px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full ${
+            activeItem === "Invoices" && "bg-gray-700 text-white"
+          }`}
         >
           <FaFileInvoice />
           <li>Invoices</li>
         </Link>
         <Link
           to="/settings"
-          className="px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full"
+          onClick={() => handleItemClick("Settings")}
+          className={`px-4 items-center justify-start pl-10 gap-2 flex flex-row py-2 hover:bg-gray-700 hover:text-white mx-4 rounded-full ${
+            activeItem === "Settings" && "bg-gray-700 text-white"
+          }`}
         >
           <FaCog />
           <li>Settings</li>
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,11 +5,12 @@ import { HiUserGroup } from "react-icons/hi";
 import { LuBaggageClaim } from "react-icons/lu";
 import { FaDollarSign } from "react-icons/fa";
 
-
 function Dashboard() {
   return (
     <div>
-        <h1 className="text-2xl font-bold my-6 ml-10">Welcome Back <span className="text-red-300">Mogeni!</span></h1>
+      <h1 className="text-2xl font-bold my-6 ml-10">
+        Welcome Back <span className="text-red-300">Mogeni!</span>
+      </h1>
       <div className="flex flex-row ">
         <div className="flex flex-row mx-auto gap-10 p-4 justify-start items-center rounded-lg shadow-lg bg-red-300 w-60">
           <div className="flex flex-col space-y-4">
@@ -56,6 +57,47 @@ function Dashboard() {
           </div>
         </div>
       </div>
+      <table className="rounded-lg bg-slate-300 mt-20 h-60 w-full ">
+        <thead className="p-4 justify-start ">
+          <tr>
+            <th>Customer</th>
+            <th>Package</th>
+            <th>Enrolled</th>
+            <th>Progress</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jane Cooper</td>
+            <td>Regular</td>
+            <td>Jan 28,2023</td>
+            <td>80%</td>
+            <td>5 stars</td>
+          </tr>
+          <tr>
+            <td>Wade Warren</td>
+            <td>Professional</td>
+            <td>Jan 28,2023</td>
+            <td>25%</td>
+            <td>4 stars</td>
+          </tr>
+          <tr>
+            <td>Leslie Alexander</td>
+            <td>Enterprise</td>
+            <td>Jan 28,2023</td>
+            <td>30%</td>
+            <td>5 starts</td>
+          </tr>
+          <tr>
+            <td>Fider Costa</td>
+            <td>Regular</td>
+            <td>Jan 28,2023</td>
+            <td>40%</td>
+            <td>3 stars</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
