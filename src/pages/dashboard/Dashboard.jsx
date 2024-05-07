11289ba3784f2c8751b907d2c88b28cb81dcd7df
Sherile <sherilecheptoo1@gmail.com>
2024-05-08 00:45:59 +0300

    map data

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,103 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import MainLayout from "../../layout/MainLayout";
 import { BsGraphUp } from "react-icons/bs";
 import { HiUserGroup } from "react-icons/hi";
 import { LuBaggageClaim } from "react-icons/lu";
 import { FaDollarSign } from "react-icons/fa";
-
+// import data from "./data";
+import cards from "./Data";
 function Dashboard() {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
   return (
     <div>
       <h1 className="text-2xl font-bold my-6 ml-10">
         Welcome Back <span className="text-red-300">Mogeni!</span>
       </h1>
       <div className="flex flex-row ">
-        <div className="flex flex-row mx-auto gap-10 p-4 justify-start items-center rounded-lg shadow-lg bg-red-300 w-60">
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-semibold">Visitors Today</h2>
-            <p>5600</p>
+        {cards.map((item) => (
+          <div className="flex flex-row mx-auto gap-10 p-4 justify-start items-center rounded-lg shadow-lg bg-red-300 w-60">
+            <div className="flex flex-col space-y-4">
+              <h2 className="font-semibold">{item.title}</h2>
+              <p>{item.value}</p>
+            </div>
+            <div>
+              <p className="text-white bg-red-100 rounded-full p-2">
+                {item.icon}
+              </p>
+            </div>
           </div>
-          <div>
-            <p className="text-white bg-red-100 rounded-full p-2">
-              <BsGraphUp className="text-xl" />
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row mx-auto gap-10 p-4 justify-start items-center rounded-lg shadow-lg bg-slate-400 w-60">
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-semibold">New Members</h2>
-            <p>250</p>
-          </div>
-          <div>
-            <p className="text-white bg-slate-200 rounded-full p-2">
-              <HiUserGroup className="text-xl" />
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row mx-auto gap-10 p-4 justify-start items-center rounded-lg shadow-lg bg-sky-400 w-60">
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-semibold">New Orders</h2>
-            <p>852</p>
-          </div>
-          <div>
-            <p className="text-white bg-sky-200 rounded-full p-2">
-              <LuBaggageClaim className="text-xl" />
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row gap-10 p-4 mx-auto justify-start items-center rounded-lg shadow-lg bg-teal-300 w-60">
-          <div className="flex flex-col space-y-4">
-            <h2 className="font-semibold">Total Earnings</h2>
-            <h1>$1350</h1>
-          </div>
-          <div>
-            <p className="text-white bg-teal-100 rounded-full p-2">
-              <FaDollarSign className="text-xl" />
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
-      <table className="rounded-lg bg-slate-300 mt-20 h-60 w-full ">
-        <thead className="p-4 justify-start ">
-          <tr>
-            <th>Customer</th>
-            <th>Package</th>
-            <th>Enrolled</th>
-            <th>Progress</th>
-            <th>Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Jane Cooper</td>
-            <td>Regular</td>
-            <td>Jan 28,2023</td>
-            <td>80%</td>
-            <td>5 stars</td>
-          </tr>
-          <tr>
-            <td>Wade Warren</td>
-            <td>Professional</td>
-            <td>Jan 28,2023</td>
-            <td>25%</td>
-            <td>4 stars</td>
-          </tr>
-          <tr>
-            <td>Leslie Alexander</td>
-            <td>Enterprise</td>
-            <td>Jan 28,2023</td>
-            <td>30%</td>
-            <td>5 starts</td>
-          </tr>
-          <tr>
-            <td>Fider Costa</td>
-            <td>Regular</td>
-            <td>Jan 28,2023</td>
-            <td>40%</td>
-            <td>3 stars</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="p-4">
+        <table className="rounded-lg bg-slate-300 mt-20 h-60 w-full  ">
+          <thead className="p-4 ">
+            <tr className="">
+              {/*<th className="text-start">Select</th>*/}
+              <th className="text-start">Customer</th>
+              <th className="text-start">Package</th>
+              <th className="text-start">Enrolled</th>
+              <th className="text-start">Progress</th>
+              <th className="text-start">Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="items-center justify-start gap-2 flex ">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                />
+                <p>Jane Cooper</p>
+              </td>
+              <td>Regular</td>
+              <td>Jan 28,2023</td>
+              <td>80%</td>
+              <td>5 stars</td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-6 w-6 text-indigo-600 transition duration-150 ease-in-out"
+                />
+              </td>
+
+              <td>Wade Warren</td>
+              <td>Professional</td>
+              <td>Jan 28,2023</td>
+              <td>25%</td>
+              <td>4 stars</td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-6 w-6 text-indigo-600 transition duration-150 ease-in-out"
+                />
+              </td>
+              <td>Leslie Alexander</td>
+              <td>Enterprise</td>
+              <td>Jan 28,2023</td>
+              <td>30%</td>
+              <td>5 starts</td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-6 w-6 text-indigo-600 transition duration-150 ease-in-out"
+                />
+              </td>
+              <td>Fider Costa</td>
+              <td>Regular</td>
+              <td>Jan 28,2023</td>
+              <td>40%</td>
+              <td>3 stars</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/pages/dashboard/Data.js
@@ -0,0 +1,19 @@
+import { BsGraphUp } from "react-icons/bs";
+import { HiUserGroup } from "react-icons/hi";
+
+const cards = [
+  {
+    id: 1,
+    title: "Visitors Today",
+    value: 5600,
+    // icon: <BsGraphUp className="text-xl" />,
+  },
+  {
+    id: 2,
+    title: "New Users",
+    value: 230,
+    // icon: <HiUserGroup className="text-xl" />,
+  },
+];
+
+export default cards;
